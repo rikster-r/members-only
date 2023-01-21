1385127dc53626139b5a33b19b7cc922e18f3743
rikster-r <seitdanial43@gmail.com>
2023-01-21 17:21:17 +0600

    Remove users router

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -79,7 +78,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
